4f8b818168933262383ea15e6c701be7b1a47026
A <201318652@ingenieria.usac.edu.gt>
2022-09-16 01:22:45 -0600

    hechos depto tabla

--- BestHotel.pl
+++ BestHotel.pl
@@ -1,4 +1,25 @@
 % --- Base de conocimiento ---.
 
 % --- Hechos departamento ---.
-departamento(1, peten, ).
+departamento(d1, peten, 8, espanol, tropical).
+departamento(d2, izabal, 5, espanol, calor).
+departamento(d3, alta_verapaz, 6, espanol, frio).
+departamento(d4, quiche, 5, katchikel, frio).
+departamento(d5, huehuetenango, 7, espanol, frio).
+departamento(d6, escuintla, 2, espanol, calor).
+departamento(d7, san_marcos,3, ingles, calor).
+departamento(d8, jutiapa, 4, katchikel, calor).
+departamento(d9, baja_verapaz, 5, ingles, templado).
+departamento(d10, santa_rosa, 4, ingles, calor).
+departamento(d11, zacapa, 3, ingles, calor).
+departamento(d12, suchitepequez, 5, espanol, templado).
+departamento(d13, chiquimula, 4, katchikel, calor).
+departamento(d14, guatemala, 0, ingles, templado).
+departamento(d15, jalapa, 4, ingles, calor).
+departamento(d16, chimaltenango, 3, katchikel, calor).
+departamento(d17, quetzaltenango, 3, ingles, frio).
+departamento(d18, el_progreso, 4, katchikel, calor).
+departamento(d19, retalhuleu, 4, ingles, calor).
+departamento(d20, solola, 6, ketchi, frio).
+departamento(d21, totonicapan, 5, ingles, templado).
+departamento(d22, sacatepequez, 2, espanol, templado).
